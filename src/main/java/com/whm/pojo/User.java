e8e6373db99903f6f47043c8a7d1c0da460fd372
Alina <930012729@qq.com>
2018-04-03 15:48:12 +0800

    添加实体类注解

--- src/main/java/com/whm/pojo/User.java
+++ src/main/java/com/whm/pojo/User.java
@@ -1,42 +1,15 @@
 package com.whm.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 	private String id;
 	private String name;
 	private String age;
-		
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getAge() {
-		return age;
-	}
-	public void setAge(String age) {
-		this.age = age;
-	}
-	public User() {
-		// TODO Auto-generated constructor stub
-	}
-	public User(String id, String name, String age) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.age = age;
-	}
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
-	}
-	
 
 }
